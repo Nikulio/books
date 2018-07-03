ed375cac374ac778bfecd9d491428c646a2e26e3
Nikita Shchypylov <shchypylov@gmail.com>
2018-07-03 14:23:56 +0300

    path update

--- server/server.js
+++ server/server.js
@@ -25,8 +25,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/", BookSchemaRouter);
-app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(express.static(path.join(__dirname, '../build')));
 
 const server = http.createServer(app);
 
