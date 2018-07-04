ae9c0672400205c08bde9363ef5abfc8b44e4717
Nikita Shchypylov <shchypylov@gmail.com>
2018-07-04 15:19:48 +0300

    new styles

--- src/сomponents/Home/index.js
+++ src/сomponents/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchBooks } from "../../actions";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Book from "../Book";
 
 
